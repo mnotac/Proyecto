a5a9d156bc7b0c4310c62a1e56f425cdb3ed4de7
Paula <PaulaFV524@gmail.com>
2023-02-08 16:00:48 +0100

    ComprobarFalses

---
+++ LucesFora.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class LucesFora {
+
+public int dimension;
+
+public int dimensionRespaldo;
+
+
+public int getDimension() {
+return dimension;
+}
+
+public void setDimension(int dimension) {
+this.dimension = dimension;
+}
+
+public int getDimensionRespaldo() {
+return dimensionRespaldo;
+}
+
+public void setDimensionRespaldo(int dimensionRespaldo) {
+this.dimensionRespaldo = dimensionRespaldo;
+}
+
+public void Imprimir() {
+    System.out.println();
+}
+}
+
+public void ComprobarFalse(){
+
+
+
+//Tras invertir las posiciones deberíamos comprobar si la matriz está toda a False (todas las luces apagadas),
+//en cuyo caso el usuario habrá ganado y la partida termina. En caso contrario realizaremos una nueva iteración.
+for(int i=0;i>dimension;i++){
+    for(int a=0;a>dimensionRespaldo;a++){
+        if()
+    }
+}
+}
