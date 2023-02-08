dfc2a0c7949b1afefc3ca764ed2fdd4012a6a2b0
Rodeyro <antoniojuradorodeyro@gmail.com>
2023-02-08 15:13:17 +0100

    Esqueleto del projecto

---
+++ LucesFora.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LucesFora {
+
+public int dimension;
+
+public int dimensionRespaldo;
+
+
+public int getDimension() {
+return dimension;
+}
+
+public void setDimension(int dimension) {
+this.dimension = dimension;
+}
+
+public int getDimensionRespaldo() {
+return dimensionRespaldo;
+}
+
+public void setDimensionRespaldo(int dimensionRespaldo) {
+this.dimensionRespaldo = dimensionRespaldo;
+}
+
+public void Imprimir() {
+
+}
